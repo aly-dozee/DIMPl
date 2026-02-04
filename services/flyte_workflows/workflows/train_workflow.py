59a77da11076a002359be7c63528595d4504f73a
aly <alymustafa@dozee.io>
2026-02-04 16:12:45 +0530

    starting from scratch due to requirement changes

--- services/flyte_workflows/workflows/train_workflow.py
+++
@@ -1,9 +0,0 @@
-# flyte_workflows/workflows/train_workflows.py
-
-from flytekit import workflow
-
-from services.flyte_workflows.tasks.train_task import train_model
-
-@workflow
-def train_wf(params: dict) -> str:
-    return train_model(params=params)
